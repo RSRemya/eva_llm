802e9c73f35f2c91f8c13d728ae816780d8df0b7
Remya R S <117028538+RSRemya@users.noreply.github.com>
2025-03-27 16:11:09 +0300

    Add files via upload

---
+++ Eva_Utilities/Util.cs
@@ -0,0 +1,27 @@
+﻿namespace Eva_Utilities
+{
+
+    using UglyToad.PdfPig;
+    public static class Util
+    {
+        public static string ExtractTextFromPdfs(string pdfDirPaths)
+        {
+            string textContent = "";
+            DirectoryInfo d = new DirectoryInfo(pdfDirPaths);
+
+            foreach (var pdfFilePath in d.GetFiles().Where(f=>f.Extension==".pdf").Select(f => f.FullName))
+            {
+                using (var document = PdfDocument.Open(pdfFilePath))
+                {
+                    foreach (var page in document.GetPages())
+                    {
+                        textContent += page.Text; // Extract text from each page
+                    }
+                }
+            }
+
+            return textContent;
+        }
+
+    }
+}
