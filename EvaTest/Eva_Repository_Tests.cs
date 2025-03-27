36283a65ca0508e4ef7eef32966b0ce22eababe5
Remya R S <117028538+RSRemya@users.noreply.github.com>
2025-03-27 16:11:46 +0300

    Add files via upload

---
+++ EvaTest/Eva_Repository_Tests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YourNamespace.Tests
+{
+    [TestClass]
+    public class MyDbContextTests
+    {
+        private DbContextOptions<MyDbContext> GetInMemoryDbContextOptions()
+        {
+            return new DbContextOptionsBuilder<MyDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [TestMethod]
+        public void CanAddUser()
+        {
+            // Arrange
+            var options = GetInMemoryDbContextOptions();
+            using var context = new MyDbContext(options);
+
+            var user = new User { UserId = Guid.NewGuid(), UserAnonymousName = "AnonymousUser1" };
+
+            // Act
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual(1, context.Users.Count());
+            Assert.AreEqual("AnonymousUser1", context.Users.Single().UserAnonymousName);
+        }
+
+        [TestMethod]
+        public void CanAddBotStackConfigModel()
+        {
+            // Arrange
+            var options = GetInMemoryDbContextOptions();
+            using var context = new MyDbContext(options);
+
+            var user = new User { UserId = Guid.NewGuid(), UserAnonymousName = "AnonymousUser1" };
+            var botConfig = new BotStackConfigModel { BotId = Guid.NewGuid(), ApiKey = "SomeApiKey", UserId = user.UserId };
+
+            // Act
+            context.Users.Add(user);
+            context.BotStackConfigModels.Add(botConfig);
+            context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual(1, context.Users.Count());
+            Assert.AreEqual(1, context.BotStackConfigModels.Count());
+            Assert.AreEqual("SomeApiKey", context.BotStackConfigModels.Single().ApiKey);
+            Assert.AreEqual(user.UserId, context.BotStackConfigModels.Single().UserId);
+        }
+
+        [TestMethod]
+        public void CanAddDiaryEntry()
+        {
+            // Arrange
+            var options = GetInMemoryDbContextOptions();
+            using var context = new MyDbContext(options);
+
+            var user = new User { UserId = Guid.NewGuid(), UserAnonymousName = "AnonymousUser1" };
+            var diaryEntry = new DiaryEntry { UserId = user.UserId, DiaryEntryDate = DateTime.UtcNow, Entry = "Today was a good day." };
+
+            // Act
+            context.Users.Add(user);
+            context.DiaryEntries.Add(diaryEntry);
+            context.SaveChanges();
+
+            // Assert
+            Assert.AreEqual(1, context.Users.Count());
+            Assert.AreEqual(1, context.DiaryEntries.Count());
+            Assert.AreEqual("Today was a good day.", context.DiaryEntries.Single().Entry);
+            Assert.AreEqual(user.UserId, context.DiaryEntries.Single().UserId);
+        }
+    }
+}
---
+++ EvaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EvaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EvaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30059818-44dd-4dae-b2e9-2fabe17a1b88")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
